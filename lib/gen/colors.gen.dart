5ba7f33da6ab5c8035792d2c26206c96151e6245
Kharisma Wardhana <kharisma.wardhana@Kharismas-MacBook-Pro.local>
2025-06-21 18:06:51 +0700

    generate assets

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,45 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #1B264B
+  static const Color darkBlue = Color(0xFF1B264B);
+
+  /// Color: #57636C
+  static const Color darkGrey = Color(0xFF57636C);
+
+  /// Color: #E17E05
+  static const Color darkOrange = Color(0xFFE17E05);
+
+  /// Color: #4A65BF
+  static const Color lightBlue = Color(0xFF4A65BF);
+
+  /// Color: #D8D8D8
+  static const Color lightGrey = Color(0xFFD8D8D8);
+
+  /// Color: #FBA337
+  static const Color lightOrange = Color(0xFFFBA337);
+
+  /// Color: #16AF97
+  static const Color primary = Color(0xFF16AF97);
+
+  /// Color: #399E5A
+  static const Color secondary = Color(0xFF399E5A);
+
+  /// Color: #FFFFFF
+  static const Color white = Color(0xFFFFFFFF);
+
+  /// Color: #E5E5E5
+  static const Color whiteBackground = Color(0xFFE5E5E5);
+}
